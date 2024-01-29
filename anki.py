f309f7037b3e4d571795d9e4e507d72578019256
yrikomuro <155485209+yrikomuro@users.noreply.github.com>
2024-01-29 13:26:40 -0800

    Update anki.py

--- anki.py
+++ anki.py
@@ -1,6 +1,5 @@
 # get 'bad request' from Safari, if change Card Type from basic or reversed to some other type
-
-
+import selenium.common
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,6 +7,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from definitii import backup_title_list, backup_entry_list
+
+# for roman telega
+login_mode = False
+tries = 0
 
 
 def add_card():
@@ -19,17 +22,24 @@ def add_card():
     driver.get('https://ankiweb.net/account/login')
 
 # logging in using entered email and password
+
+    user_info = open('user_info', 'r')
+    email_input = user_info.readline()
+    pasword_input = user_info.readline().strip('\n')
     email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@placeholder='Email']")))
-    email.send_keys('')
+    email.send_keys(email_input)
 
     password = driver.find_element(By.XPATH, "//*[@placeholder='Password']")
-    password.send_keys('')
+    password.send_keys(pasword_input)
 
 # clicking the login button
-    login = driver.find_element(By.XPATH, "//*[@class='btn btn-primary btn-lg']")
-    login.click()
-    add_button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Add')))
-    add_button.click()
+    try:
+        login = driver.find_element(By.XPATH, "//*[@class='btn btn-primary btn-lg']")
+        login.click()
+        add_button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Add')))
+        add_button.click()
+    except selenium.common.ElementNotInteractableException:
+        return 'Invalid login! (Type ‹login› to reauthorize)'
 
 # changing card type for 'basic'
     try:
@@ -47,3 +57,4 @@ def add_card():
         card_ready.click()
         time.sleep(1)
         driver.close()
+
